ddb20004c91a55d96314cdaf6730022d24f2c408
imasshaWe <imasha98.we@gmail.com>
2023-12-16 15:36:12 +0530

    feat: Login UI

--- src/app/screens/LoginScreen.tsx
+++ src/app/screens/LoginScreen.tsx
@@ -2,8 +2,28 @@ import React from "react";
 
 export const LoginScreen: React.FC = () => {
   return (
-    <div>
-      <h1>Login Screen</h1>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="flex flex-col justify-center items-center w-96 h-96 bg-white rounded-lg shadow-lg">
+        <h1 className="text-4xl font-bold text-gray-800">Login</h1>
+        <form className="flex flex-col justify-center items-center w-full">
+          <input
+            className="w-3/4 h-10 px-2 mt-4 text-gray-700 bg-gray-200 rounded-lg focus:outline-none focus:bg-white focus:shadow-md"
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            className="w-3/4 h-10 px-2 mt-4 text-gray-700 bg-gray-200 rounded-lg focus:outline-none focus:bg-white focus:shadow-md"
+            type="password"
+            placeholder="Password"
+          />
+          <button
+            className="w-3/4 h-10 mt-4 text-white bg-blue-500 rounded-lg hover:bg-blue-700"
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
