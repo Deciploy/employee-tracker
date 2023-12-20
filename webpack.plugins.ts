056d9c0cea050eba071b0524b961605e14c88bc9
imasshaWe <imasha98.we@gmail.com>
2023-12-20 18:13:21 +0530

    Refactor

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,20 +1,30 @@
-import type IForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const DotenvFlow = require('dotenv-flow-webpack');
 
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
-    logger: "webpack-infrastructure",
+    logger: 'webpack-infrastructure',
   }),
   new CopyWebpackPlugin({
     patterns: [
       {
-        from: "src/assets/images",
-        to: "assets/images",
+        from: 'src/assets/images',
+        to: 'assets/images',
       },
     ],
+  }),
+  new DotenvFlow({
+    default_node_env: 'development',
+    safe: false,
+    system_vars: true,
+    silent: false,
+    encoding: 'utf8',
+    defaults: false,
   }),
 ];
