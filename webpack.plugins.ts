025730992bebf2de8746758f70a5fe9ec91bcf9d
imasshaWe <imasha98.we@gmail.com>
2023-12-20 13:17:20 +0530

    installed copy-webpack-plugin

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,10 +1,20 @@
-import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import type IForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
-    logger: 'webpack-infrastructure',
+    logger: "webpack-infrastructure",
+  }),
+  new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: "src/assets/images",
+        to: "assets/images",
+      },
+    ],
   }),
 ];
