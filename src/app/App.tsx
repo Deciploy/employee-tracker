d3c45d787f945ebc4f9ebff328417362f34d0640
imasshaWe <imasha98.we@gmail.com>
2023-12-21 20:16:11 +0530

    refact: remove console logs

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,8 +8,6 @@ import { AuthState } from 'react-auth-utils/src/lib/types';
 import { MainScreen } from './screens/MainScreen';
 import { LoginScreen } from './screens/LoginScreen';
 
-console.log(process.env.NX_APP_API_BASE_URL);
-
 const AppRoutes = () => (
   <HashRouter basename="/">
     <Routes>
