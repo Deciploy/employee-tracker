995aac39f369e1ee708a219dcdebf60cb8891a78
imasshaWe <imasha98.we@gmail.com>
2023-12-27 14:34:50 +0530

    feat: timer

--- src/app/screens/MainScreen.tsx
+++ src/app/screens/MainScreen.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAuth } from 'react-auth-utils';
 import { User } from '../models/User';
 import { Avatar } from './components/Avatar';
+import Moment from 'react-moment';
+import { Timer } from './components/Timer';
 
 export const MainScreen: React.FC = () => {
   const { user, signOut } = useAuth<User>();
+
   const [userMenuOpen, setUserMenuOpen] = React.useState(false);
+  const [tracking, setTracking] = React.useState(false);
+  const [time, setTime] = React.useState<number>(0);
 
   const handleLogout = () => {
     signOut();
   };
 
+  useEffect(() => {
+    if (tracking) {
+      const interval = setInterval(() => {
+        setTime((prev) => prev + 1);
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [tracking]);
+
   return (
-    <div className="flex">
+    <div className="flex flex-col h-screen">
       <nav className="flex flex-col w-screen items-end p-2">
         <Avatar
           fullName={user?.fullName}
@@ -41,6 +56,38 @@ export const MainScreen: React.FC = () => {
           </div>
         )}
       </nav>
+
+      <div className="flex flex-1 flex-col justify-center gap-4">
+        <div className="flex w-full justify-center">
+          <h3 className="text-xl text-secondary-500 font-medium">
+            <Moment format="dddd, D MMMM YYYY" />
+          </h3>
+        </div>
+
+        <div className="flex w-full justify-center">
+          <h1 className="text-6xl text-primary-500 font-bold">
+            <Timer seconds={time} />
+          </h1>
+        </div>
+
+        <div className="flex w-full justify-center">
+          {tracking ? (
+            <button
+              className="bg-warning-500 hover:bg-warning-600 text-white uppercase font-bold py-4 px-8 rounded-full w-64 h-18"
+              onClick={() => setTracking(false)}
+            >
+              Clock out
+            </button>
+          ) : (
+            <button
+              className="bg-primary-500 hover:bg-primary-600 text-white uppercase font-bold py-4 px-8 rounded-full w-64 h-18"
+              onClick={() => setTracking(true)}
+            >
+              Clock in
+            </button>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
