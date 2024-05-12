2a6d82354d5b437000f2cbcad7e6eaa1778fe4ff
imasshaWe <imasha98.we@gmail.com>
2024-05-12 22:47:24 +0530

    chore: Remove unused worker files and database module

--- src/app/workers/db/db.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'main.db',
-});
-
-module.exports = sequelize;
