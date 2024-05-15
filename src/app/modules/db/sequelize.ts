061ba803fca9d8275c02c0cd3a64aa2cde869132
imasshaWe <imasha98.we@gmail.com>
2024-05-15 21:14:37 +0530

    Update sequelize configuration to use environment variables for database credentials

--- src/app/modules/db/sequelize.ts
+++ src/app/modules/db/sequelize.ts
@@ -1,6 +1,16 @@
+import sqlite3 from "sqlite3";
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: "main.db",
-});
+const NAME = process.env.LOCAL_DB_NAME;
+const USERNAME = process.env.LOCAL_DB_USERNAME;
+const PASSWORD = process.env.LOCAL_DB_PASSWORD;
+
+export const sequelize = new Sequelize(
+    NAME,
+    USERNAME,
+    PASSWORD,
+    {
+        dialect: "sqlite",
+        dialectModule: sqlite3
+    }
+);
